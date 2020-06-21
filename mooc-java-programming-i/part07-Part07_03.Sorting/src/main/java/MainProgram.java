1a3860a7194b996dda3627c7fcbab1d6742ceb4f
chadwyck242 <chadwyck.codeart@gmail.com>
2020-06-21 12:22:07 -0600

    Add commenting to solution code for Sort methods

--- mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part07-Part07_03.Sorting/src/main/java/MainProgram.java
@@ -8,18 +8,19 @@ public class MainProgram {
         int[] array = {55, 11, 1, 99, 3, 12, 1, 8, 33, 2};
         int[] array2 = {-1, 3, 1, 2};
         int[] array3 = {8, 3, 7, 9, 1, 2, 4};
-//        int mostSmallest = smallest(array);
-//        int indexSmallest = indexOfSmallest(array);
-//        int indexFrom = indexOfSmallestFrom(array2, 0);
-//        System.out.println("Smallest value in array: " + mostSmallest);
-//        System.out.println("Index of smallest value in array: " + indexSmallest);
-//        System.out.println("Index of smallest from start index: " + indexFrom);
-//        MainProgram.swap(array, 1, 4);
-//        System.out.println(Arrays.toString(array));
+        int mostSmallest = smallest(array);
+        int indexSmallest = indexOfSmallest(array);
+        int indexFrom = indexOfSmallestFrom(array2, 0);
+        System.out.println("Smallest value in array: " + mostSmallest);
+        System.out.println("Index of smallest value in array: " + indexSmallest);
+        System.out.println("Index of smallest from start index: " + indexFrom);
+        MainProgram.swap(array, 1, 4);
+        System.out.println(Arrays.toString(array));
         MainProgram.sort(array3);
 
     }
 
+    // Returns the smallest value of an array
     public static int smallest(int[] array) {
         int smallest = array[0];
 
@@ -31,6 +32,7 @@ public class MainProgram {
         return smallest;
     }
 
+    // Gives the index of the smalles value in an array
     public static int indexOfSmallest(int[] array) {
         int smallest = array[0];
         int index = 0;
@@ -45,6 +47,7 @@ public class MainProgram {
         return index;
     }
 
+    // Returns index of the smallest from given start location: 
     public static int indexOfSmallestFrom(int[] array, int startIndex) {
         int index = startIndex;
         int smallest = array[index];
@@ -59,12 +62,15 @@ public class MainProgram {
         return index;
     }
 
+    // Swap method: swap two values of the given indices
     public static void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
     }
 
+    // Sort method: sort an array from smallest value to largest value
+    // Uses some of the methods written above
     public static void sort(int[] array) {
         int index = 0;
         System.out.println(Arrays.toString(array));
